366a60753d87b65f0445983fae07826893b26efb
dev2 <dev2@scube.net.in>
2024-09-09 17:02:54 +0530

    commit

--- detect.py
+++ detect.py
@@ -1,12 +1,10 @@
 import argparse
 import time
 from pathlib import Path
-
 import cv2
 import torch
 import torch.backends.cudnn as cudnn
 from numpy import random
-
 from models.experimental import attempt_load
 from utils.datasets import LoadStreams, LoadImages
 from utils.general import check_img_size, check_requirements, check_imshow, non_max_suppression, apply_classifier, \
@@ -14,7 +12,6 @@ from utils.general import check_img_size, check_requirements, check_imshow, non_
 from utils.plots import plot_one_box
 from utils.torch_utils import select_device, load_classifier, time_synchronized, TracedModel
 import streamlit as st
-
 from PIL import Image
 import tempfile
 
